9c574bc517c763fe4eeeb1a9264e2496955ecfd9
Leah Nelson and Sky Rousse <leah+sky@>
2016-09-26 16:00:52 -0700

    send to multiple recipients

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -30,6 +30,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:to, :from, :body)
+    params.require(:message).permit(:from, :body, to:[])
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,22 +1,24 @@
 class Message < ActiveRecord::Base
-  before_create :send_message
+    before_create :send_message
 
 private
   def send_message
     begin
-    response = RestClient::Request.new(
-      :method => :post,
-      :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
-      :user => ENV['TWILIO_ACCOUNT_SID'],
-      :password => ENV['TWILIO_AUTH_TOKEN'],
-      :payload => { :Body => body,
-                    :To => to,
-                    :From => from }
-    ).execute
+JSON.parse(to).each do |recipient|
+        response = RestClient::Request.new(
+          :method => :post,
+          :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
+          :user => ENV['TWILIO_ACCOUNT_SID'],
+          :password => ENV['TWILIO_AUTH_TOKEN'],
+          :payload => { :Body => body,
+                        :To => recipient,
+                        :From => from }
+        ).execute
+      end
   rescue RestClient::BadRequest => error
     message = JSON.parse(error.response)['message']
     errors.add(:base, message)
-      false
+      throw(:abort)
     end
   end
 end
